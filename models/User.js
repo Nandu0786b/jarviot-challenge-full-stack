1a08c60180a308d163422c3a870912c14a5c3a82
Nand Kishore Ola <nandkishoreola0786b@gmail.com>
2023-09-06 01:31:25 +0530

    mongodb mosdels setup

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    refresh_token: String,
+    access_token: String,
+  },
+  { timestamps: true } //it will give automatically date once it's created
+);
+mongoose.pluralize(null);
+const User = mongoose.model("User", UserSchema);
+export default User;
